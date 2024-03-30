a1f2794b29b2810d4b23d0e53496fde8f56ab241
Zizhen Luo s3839954 <zizhenluo2328@gmail.com>
2024-03-30 23:56:31 +1100

    add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { router } from "./routes/index.js";
 import connectToDB from "./utils/db/dbService.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 connectToDB();
 
+app.use(cors());
 app.use(router);
 
 app.listen(PORT, () => {
